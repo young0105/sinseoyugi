ce312aba441690a14f67faee54c7a8fcba4a6cc6
young0105 <young_0105@naver.com>
2021-02-19 10:28:51 +0900

    신서유기 업로드1
    업로드1_선영

---
+++ season.js
@@ -0,0 +1,29 @@
+$('.detail').load('season/season8.html')
+
+var sw=0
+
+$('.thum_season li a').click(function(e){
+    e.preventDefault()
+
+    var href= $(this).attr('href')
+    $('.detail').load(href)
+    sw=!sw
+    if(sw==1){
+        $('.right').animate({marginLeft:'-50%'},500)
+        $('.arrow').transition({rotate:'-=180deg'})
+    }
+    else{
+        $('.right').animate({marginLeft:'0%'},500)
+    }    
+})
+
+$('.arrow').click(function(e){
+    $(this).find('img').transition({rotate:'-=180deg'})
+    sw=!sw
+    if(sw==1){
+        $('.right').animate({marginLeft:'-50%'},500)
+    }
+    else{
+        $('.right').animate({marginLeft:'0%'},500)
+    }    
+})
---
+++ store.js
@@ -0,0 +1,34 @@
+var $grid = $('.grid').isotope({
+    function () {
+        $grid.isotope({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            masonry: {
+            columnWidth: '.grid-sizer'
+          }
+        });
+    }// options
+});
+
+$grid.imagesLoaded().progress(function () {
+    $grid.isotope('layout');
+});
+
+$('.store_nav li').on('click', function () {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: filterValue
+    });
+});
+
+// var menubtnTop=$('.store_nav').offset().top
+// $(window).scroll(function(){
+//     var scrTop=$(window).scrollTop()
+//     if(menubtnTop<scrTop){
+//         $('.store_nav').addClass('Fix')
+//     }//if
+//     else{
+//         $('.store_nav').removeClass('Fix')
+//     }//else
+
+// })
